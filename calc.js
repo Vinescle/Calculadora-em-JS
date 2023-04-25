229d64180e501007e27066790045e1a8c4859316
Vini <Vinicius.bmartinho40@gmail.com>
2023-04-25 20:36:28 -0300

    Calculadora Corrigida

--- calc.js
+++ calc.js
@@ -1,48 +1,69 @@
-
-// declaração da variável global document denominada como d
 let d = document;
+let resultado = "";
+let regexSinais = /[\+\-\*\/]/;
+let ultimoBotao = null;
 
-function insert(num)
-{
-    
-	//Pegar o valor do objeto HTML resultado e armazena na variável número
-	var numero = d.getElementById('resultado').innerHTML;
-	//Faz a concatenação da variável com o valor numérico indicado pela tecla
-    d.getElementById('resultado').innerHTML = numero + num;
-	
-	
-	//Forma avançada de representar o mesmo algoritmo do bloco acima
-	//d.getElementById('resultado').innerHTML += num;
-	
+function insert(num) {
+	let ultimoCaractere = resultado.slice(-1);
+	let valorDigitadoEhOperador = regexSinais.test(num);
+
+	if(ultimoBotao == "=") {
+		clean();
+		ultimoBotao = null;
+	}
+
+	if (ultimoCaractere === "" && valorDigitadoEhOperador) {
+		// Ignora o operador se o resultado estiver vazio
+		return;
+	} else if (valorDigitadoEhOperador && regexSinais.test(ultimoCaractere)) {
+		// Substitui o último operador por um novo operador
+		resultado = resultado.slice(0, -1);
+	} else if (ultimoCaractere === "=" && !valorDigitadoEhOperador) {
+		// Limpa o resultado se um número for digitado após o resultado
+		resultado = "";
+	}
+
+	// Concatena o novo caractere ao resultado
+	resultado += num;
+	d.getElementById("resultado").innerHTML = resultado;
 }
 
-function clean()
-{
-    
-	// Limpa todos os valores contidos no objeto Html resultado
-	d.getElementById('resultado').innerHTML = "";
+function clean() {
+	resultado = "";
+	d.getElementById("resultado").innerHTML = "";
 }
-function back()
-{
-    // Armazena o conteúdo do objeto Html resultado na variável denominada como resultado
-	var resultado = d.getElementById('resultado').innerHTML;
-	// Usa a função substring para transformar a variável resultado em um vetor e subtrair uma posição do vetor, retornando o resultado para o objeto Html resultado
-    d.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
+function back() {
+	let ultimoCaractere = resultado.slice(-1);
+	resultado = resultado.slice(0, -1);
+	d.getElementById("resultado").innerHTML = resultado;
 }
-function calcular()
-{
-    // Armazena o conteúdo do objeto Html resultado na variável denominada como resultado
-	var resultado = d.getElementById('resultado').innerHTML;
-    
-	// Verifica se existe um valor na varíável
-	if(resultado)
-    {
-        // Se existe um valor é executado a função eval para calcular qualquer valor que está na variável resultado e retornado o resultado para o objeto Html resultado
-		d.getElementById('resultado').innerHTML = eval(resultado);
-    }
-    // Caso o objeto Html resultado esteja vazio é retornado um valor "Nada..."
-	else
-    {
-		d.getElementById('resultado').innerHTML = "Nada..."
-    }
+
+function calcular() {
+	let ultimoCaractere = resultado.slice(-1);
+	let regexSinais = /[\+\-\*\/]/;
+	let ultimoOperador = null;
+
+	ultimoBotao = '=';
+
+	if (resultado) {
+		let valorAtual = eval(resultado);
+		resultado = valorAtual.toString();
+		d.getElementById("resultado").innerHTML = resultado;
+
+		// Verifica se o último operador foi um sinal matemático
+		if (regexSinais.test(ultimoCaractere)) {
+			ultimoOperador = valorAtual;
+		}
+	} else {
+		d.getElementById("resultado").innerHTML = "Nada...";
+	}
+
+	// Adicione alguns logs para depurar o código
+	console.log("Resultado:", resultado);
+	console.log("Último operador:", ultimoOperador);
+}
+
+if (d.getElementById("btnCalcular")) {
+	d.getElementById("btnCalcular").addEventListener("click", calcular);
 }
